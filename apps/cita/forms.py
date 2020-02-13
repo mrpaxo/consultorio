7fdbaa78ff38d0f46e37471ed04194be622735c1
mrpaxo <mrpaxo297@gmail.com>
2020-02-13 16:42:55 -0600

    datatable etiquetas en español

--- apps/cita/forms.py
+++ apps/cita/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from apps.cita.models import Cita
+from apps.persona.models import Paciente
 
 COLOR = (
     ('#fcf12e','Amarillo'),
@@ -14,6 +15,7 @@ STATUS =((1,'Activo'),
 (3,'No Asistio'),)
 
 class CitaForm(forms.ModelForm):
+    paciente = forms.ModelChoiceField( required=True,label="Paciente",queryset=Paciente.objects.filter(estado = True).order_by('nombre'))
     inicio = forms.DateTimeField(widget=forms.DateTimeInput(format = '%d-%m-%Y %H:%M',attrs={'readonly': True}), input_formats=('%d-%m-%Y %H:%M',), required=True)
     color = forms.ChoiceField(choices = COLOR,widget=forms.Select())
     status = forms.ChoiceField(choices = STATUS,widget=forms.Select())
